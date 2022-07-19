e62488fd9a5652887472db984addec7da5cefe2a
Antonio Vicente <38213571+tonivics@users.noreply.github.com>
2022-07-19 08:47:54 -0300

    Add files via upload

---
+++ calculaMedia.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+double media(int a, int b,  int c){
+	double media = (a+b+C)/3;
+	return media;
+}
+
+int main(){
+	
+	return 0;
+}
---
+++ leInteiros.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int num1, num2, num3;
+	printf("Insira três inteiros: ");
+	scanf("%d %d %d", &num1, &num2, &num3);
+	
+	return 0;
+}
